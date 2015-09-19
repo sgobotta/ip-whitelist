6fe6b18d3cb46fb17c651c62be356f097231fa1f
Simon Rycroft <simon.rycroft@gmail.com>
2015-09-19 11:55:43 +0100

    Allowing all IPs if METEOR_SETTINGS environment variable is missing.

--- ip-whitelist-tests.js
+++ ip-whitelist-tests.js
@@ -1,5 +1,6 @@
 // Write your tests here!
 // Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+Tinytest.add('Does the IPWhitelist function return true if the METEOR_SETTINGS environment variable is undefined', function (test) {
+    var result = IPWhitelist();
+    test.equal(result, true);
 });
--- ip-whitelist.js
+++ ip-whitelist.js
@@ -1,1 +1,10 @@
-// Write your package code here!
+IPWhitelist = function() {
+
+    var meteorSettings = process.env.METEOR_SETTINGS,
+        whitelist;
+    
+    if (typeof meteorSettings === "undefined") {
+        return true;
+    }
+}
+
