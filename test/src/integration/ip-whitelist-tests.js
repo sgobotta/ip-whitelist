5bb05b4e1e8e70259879d7cf85cd8302b5ea2ca3
Simon Rycroft <simon.rycroft@gmail.com>
2015-09-28 13:22:48 +0100

    Removed integration test.

--- test/src/integration/ip-whitelist-tests.js
+++
@@ -1,4 +0,0 @@
-Tinytest.add('Does the IPWhitelist function return true if the METEOR_SETTINGS environment variable is undefined', function (test) {
-    var result = IPWhitelist();
-    //test.equal(result, true);
-});
